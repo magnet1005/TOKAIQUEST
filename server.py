4a45aab2ab9331e7f9b47b9d68a55b411fa3f838
magnet <kose1005110@gmail.com>
2025-02-21 17:16:41 +0900

    Update server.py

--- server.py
+++ server.py
@@ -236,7 +236,7 @@ def main():
     bottom_words = ", ".join(bottom())
     len_yougo = ", ".join(len_yougo())
 
-  response = output(client, model, top_words, bottom_words, len_yougo)
+  response = output(client, model, str(top_words), str(bottom_words), str(len_yougo))
         
   return jsonify({"response": chat_completion.choices[0].message.content})
 
